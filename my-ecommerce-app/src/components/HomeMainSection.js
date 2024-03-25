07b598c1ae969fbb01ec6deb986cacc8e80f1e05
FarisJanjua <144174580+FarisJanjua@users.noreply.github.com>
2024-03-25 17:01:56 -0600

    Update HomeMainSection.js
    added update

--- my-ecommerce-app/src/components/HomeMainSection.js
+++ my-ecommerce-app/src/components/HomeMainSection.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import reviews from '../data/reviews';
+import { Link } from 'react-router-dom';
 
 const HomeMainSection = () => {
     const [randomReviews, setRandomReviews] = useState([]);
@@ -18,15 +19,14 @@ const HomeMainSection = () => {
         <div>
             <h2>About Us</h2>
             {/* Company's mission and vision content */}
-            <p> 1. Expert Product Knowledge: At Laflamme company, we pride ourselves on our deep understanding of Home essential equipment. Our team consists of knowledgeable experts who are passionate about cameras and accessories. Whether you're a professional photographer or a hobbyist, we're here to provide you with expert guidance and recommendations tailored to your needs.</p>
-            <p> 2. Exceptional Customer Service: Providing outstanding customer service is our top priority. From the moment you visit our website to the delivery of your order, we strive to ensure that your experience with Laflamme company is seamless and enjoyable. Our dedicated support team is available to assist you with any inquiries or concerns, ensuring that you receive the assistance you need promptly and efficiently.</p>
-            <p> 3. Commitment to Innovation: At Laflamme company, we're committed to staying at the forefront of the essentials industry. We continuously research and evaluate the latest advancements in technology and equipment to ensure that we offer our customers the most innovative and cutting-edge products available. Whether it's the latest camera model or groundbreaking accessories, you can trust Laflamme company to provide you with the tools you need to unleash your creativity and capture stunning images.</p>
-
+            <p>At Laflamme company, we excel in home essentials, providing tailored advice and seamless customer support. Our dedication to innovation ensures we offer cutting-edge products for your creativity and photography needs.</p>
         
-            <button onClick={() => { /* Handle click event to navigate to ProductPage */ }}>
-                Shop Now
-            </button>
-
+           
+            <Link to="/products">
+                <button>
+                    Shop Now
+                </button>
+            </Link>
             <h2>Customer Reviews</h2>
 
             {randomReviews.map((review, index) => (
@@ -43,6 +43,3 @@ const HomeMainSection = () => {
             ))}
         </div>
     );
-};
-
-export default HomeMainSection;
